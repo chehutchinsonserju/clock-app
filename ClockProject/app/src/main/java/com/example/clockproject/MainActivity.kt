d9ff9c21b5e2022e5fbf820831bdda31461b68e2
tavtav1 <tavian.hall@hotmail.com>
2022-10-19 16:55:35 -0500

    Current time

---
+++ ClockProject/app/src/main/java/com/example/clockproject/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.clockproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextClock
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        var textClock = findViewById<TextClock>(R.id.textclockid)
+        var textdate = findViewById<TextClock>(R.id.textdateid)
+    }
+}
