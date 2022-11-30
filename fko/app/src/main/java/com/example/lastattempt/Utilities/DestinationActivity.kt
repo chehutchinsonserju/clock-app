9da5697db2ad97c7894705cfe90c7f1744696011
Ljacenizer <91333665+chehutchinsonserju@users.noreply.github.com>
2022-11-29 22:02:50 -0500

    Alarm and splash
    added alarm and a splash startup

---
+++ fko/app/src/main/java/com/example/lastattempt/Utilities/DestinationActivity.kt
@@ -0,0 +1,12 @@
+package com.example.lastattempt.Utilities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.lastattempt.R
+
+class DestinationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_destination)
+    }
+}
